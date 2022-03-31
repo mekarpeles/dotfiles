1f2d23287d34a5b84239f7e4cafb8dfc50245c66
Michael E. Karpeles (Mek) <michael.karpeles@gmail.com>
2022-03-30 20:07:02 -0700

    updating dotfiles

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-apt-get install git emacs tilda tmux stumpwm openssh-server chromium-browser firefox vlc audacity festival
+apt-get install git emacs tilda tmux openssh-server chromium vlc audacity festival gimp zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+echo 'ZSH_THEME="gentoo"' >> ~/.zshrc
+cp tilda/config_0 ~/.config/tilda/config_0
+cp .screenrc ~/.screenrc
 cp tmux.conf ~/.tmux.conf
 cp stumpwmrc ~/.stumpwmrc
-
